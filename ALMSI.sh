5d5fd61d21bdc8c9649abac4e9bfcb88214f7ee9
Muflone <webreg@muflone.com>
2020-12-05 21:32:53 +0100

    Added missing space

--- ALMSI.sh
+++ ALMSI.sh
@@ -5,6 +5,7 @@
 #      Author: Fabio Castelli (Muflone) <muflone@muflone.com>
 #   Copyright: 2020 Fabio Castelli
 #     License: GPL-3+
+#
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
